468ffd1651d687fc414a745874f39c15646fe908
Deawon <19999eo@naver.com>
2023-02-20 17:34:04 +0900

    re_second-commit

--- js/chat.js
+++ js/chat.js
@@ -1,22 +1,11 @@
 
 /*chatting*/
+/*채팅 보낼때 값이 사라지게하기 성공!
+아무 값이 없을 때 안보내지게 하기는 됨*/
 
 let chatPost=document.getElementById("chatPost")
 let chatUser=document.getElementById("chatUser")
 let chatSend=document.querySelector('.chatsend')
-
-chatSend=true;
-chatPost=true;
-
-function stop(){
-    if(chatUser.value==""){
-        chatSend=false;
-        chatPost=false;
-
-    }
-}
-
-stop()
 
 
 document.getElementById('chatUser').addEventListener('keydown',function(event){
@@ -26,6 +15,9 @@ document.getElementById('chatUser').addEventListener('keydown',function(event){
     }
 });
 document.querySelector('.chatsend').addEventListener('click',function(){
+    if(chatUser.value==""){
+        chatSend=none;
+    }
     document.getElementById('chatBot').value += '\n나 : '+document.getElementById('chatUser').value;
+    chatUser.value=""
 });
-
