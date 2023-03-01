782406e319f6c455eff4c75202cca815c453d15c
HYEONJI1209 <108385746+HYEONJI1209@users.noreply.github.com>
2023-03-01 21:05:40 +0900

    login-pw-eye

--- js/login.js
+++ js/login.js
@@ -1,18 +1,37 @@
-const loginCheck=()=>{
+//로그인 빈값 검사
+
+const loginCheck = () => {
     //아이디가 빈칸이라면 경고 출력
-    if(logForm.id.value==""){
+    if (logForm.id.value == "") {
         alert("아이디를 입력하세요");
         logForm.id.focus(); //포커스 커서 옮기기
         return false; //정상종료
     }
     //비밀번호가 빈칸이라면 경고 출력
-    if(logForm.pw.value==""){
+    if (logForm.pw.value == "") {
         alert("비밀번호를 입력하세요");
         logForm.pw.focus(); //포커스 커서 옮기기
         return false; //정상종료
     }
-    location.href="Index.html";  //둘다 입력되면 Index.html로
+    location.href = "Index.html";  //둘다 입력되면 Index.html로
 }
+
+
+//비밀번호 눈
+$(function () {
+    // 눈표시 클릭 시 패스워드 보이기
+    $('.eyes').on('click', function () {
+        $('.input.password').toggleClass('active');
+
+        if ($('.input.password').hasClass('active') == true) {
+            $(this).find('.fa-eye').attr('class', "fa fa-eye-slash fa-lg").parents('.input').find('#password').attr('type', "text");
+            // i 클래스                // 텍스트 보이기 i 클래스
+        }
+        else {
+            $(this).find('.fa-eye-slash').attr('class', "fa fa-eye fa-lg").parents('.input').find('#password').attr('type', 'password');
+        }
+    });
+});
 
 
 // let loginid = document.getElementById("id");
