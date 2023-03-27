84ae435b80bcba6f7c7cb55dbc6b11f9ea6417f3
HYEONJI1209 <108385746+HYEONJI1209@users.noreply.github.com>
2023-03-27 19:14:56 +0900

    윤성이 파일

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+ $('.flex a').click(function(){
+            let index = $(this).index();
+            index++;
+            $('.flex a').removeClass('active');
+            $(this).addClass('active');
+
+            $('.content').removeClass('on');
+            $('#contents'+index).addClass('on');
+        })
