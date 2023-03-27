0c723ea8330cc83f0fad98e70a5592bf1339845a
Deawon <19999eo@naver.com>
2023-03-27 21:08:25 +0900

    modal-revise

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,11 +1,13 @@
 // 모달 버튼과 모달 창을 변수로 지정
-var modalBtn = document.getElementById("modal-btn");
+//var modalBtn = document.getElementById("modal-btn");
 var modal = document.getElementById("modal");
 
 // 모달 버튼을 클릭하면 모달 창을 열도록 설정
-modalBtn.onclick = function() {
+/*modalBtn.onclick = function() {
   modal.style.display = "block";
-}
+}*/
+
+modal.style.display = "block";
 /*
 // 모달 창 내부의 X 버튼을 클릭하면 모달 창을 닫도록 설정
 var closeBtn = modal.querySelector(".close");
@@ -21,3 +23,4 @@ closeBtn.onclick = function() {
 //     modal.style.display = "none";
 //   }
 // }
+
